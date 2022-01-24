3b389585cd9467bc7c12bfa8391caf2202d33f99
liuzhangjian <liuzhangjian@uniontech.com>
2022-01-24 15:24:07 +0800

    fix: 无法搜索到对应的结果
    汉字转拼音时，当转换的拼音中存在全角符号时，字符长度计算错误
    
    Log: 修复无法搜索到对应结果的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-112132.html
    Change-Id: I41a17881f064fcfc2c0ecd71413b7ba601b1cd83

--- 3rdparty/fsearch/fsearch_utils.cpp
+++ 3rdparty/fsearch/fsearch_utils.cpp
@@ -36,11 +36,11 @@ bool convert_chinese_2_pinyin(const char *in_str, char *out_first_py, char *out_
     QString full_py;
     bool ret = GrandSearch::ChineseLetterHelper::instance()->convertChinese2Pinyin(str, first_py, full_py);
     if (ret) {
-        int full_py_len = full_py.size();
-        int first_py_len = first_py.size();
+        int full_py_len = full_py.toLocal8Bit().size();
+        int first_py_len = first_py.toLocal8Bit().size();
 
-        memcpy(out_full_py, full_py.toLocal8Bit().data(), full_py_len > FILENAME_MAX ? FILENAME_MAX : full_py_len);
-        memcpy(out_first_py, first_py.toLocal8Bit().data(), first_py_len > FILENAME_MAX ? FILENAME_MAX : first_py_len);
+        memcpy(out_full_py, full_py.toLocal8Bit().data(), full_py_len > FILENAME_MAX ? FILENAME_MAX : static_cast<size_t>(full_py_len));
+        memcpy(out_first_py, first_py.toLocal8Bit().data(), first_py_len > FILENAME_MAX ? FILENAME_MAX : static_cast<size_t>(first_py_len));
     }
 
     return ret;
