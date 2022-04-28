26231cb5d79feab83af08086cad3a6b492337ea3
Zhao Hanxi <zhaohanxi@uniontech.com>
2022-04-28 16:34:58 +0800

    chore: add ut about tail data
    add ut about tail data
    
    Log:
    
    Influence: 无
    Change-Id: Id2447dc951ef25a77fe31381ca0b206b95d4c809

--- tests/grand-search/gui/exhibition/matchresult/listview/ut_grandsearchlistdelegate.cpp
+++ tests/grand-search/gui/exhibition/matchresult/listview/ut_grandsearchlistdelegate.cpp
@@ -29,6 +29,7 @@
 #include "stubext.h"
 
 #include <DStyledItemDelegate>
+#include <DGuiApplicationHelper>
 
 #include <gtest/gtest.h>
 
@@ -221,7 +222,6 @@ TEST(GrandSearchListDelegateTest, drawSelectState)
 TEST(GrandSearchListDelegateTest, drawSearchResultText)
 {
     GrandSearchListView view;
-    GrandSearchListDelegate *delegate = view.m_delegate;
 
     QString searchGroupName(GRANDSEARCH_GROUP_FOLDER);
     MatchedItem item;
@@ -255,15 +255,222 @@ TEST(GrandSearchListDelegateTest, drawSearchResultText)
         ut_call_restore = true;
     });
 
-    delegate->drawSearchResultText(&painter, option, index);
+    bool drawTail = false;
+    stu.set_lamda(&GrandSearchListDelegate::drawTailText, [&](){
+        drawTail = true;
+    });
+
+    view.m_isPreviewItem = false;
+    view.m_delegate->drawSearchResultText(&painter, option, index);
     EXPECT_TRUE(ut_call_save);
     EXPECT_TRUE(ut_call_restore);
+    EXPECT_TRUE(drawTail);
 
     ut_call_save = false;
     ut_call_restore = false;
+    drawTail = false;
 
+    view.m_isPreviewItem = true;
     view.m_themeType = 1;
-    delegate->drawSearchResultText(&painter, option, index);
+    view.m_delegate->drawSearchResultText(&painter, option, index);
     EXPECT_TRUE(ut_call_save);
     EXPECT_TRUE(ut_call_restore);
+    EXPECT_FALSE(drawTail);
+}
+
+TEST(GrandSearchListDelegateTest, drawTailText)
+{
+    GrandSearchListView view;
+    QStyleOptionViewItem option;
+    int textMaxWidth = 500;
+    int actualStartx = 100;
+    MatchedItem item;
+    QStringList list;
+    list.append("aaa");
+    list.append("bbb");
+    QVariantHash value({{GRANDSEARCH_PROPERTY_ITEM_TAILER, list}});
+    item.extra = QVariant::fromValue(value);
+
+    MatchedItems items{item, item};
+    view.setMatchedItems(items);
+
+    QModelIndex index = view.model()->index(0, 0);
+    view.setCurrentIndex(index);
+
+    view.onSetThemeType(DGuiApplicationHelper::DarkType);
+    option.widget = &view;
+
+    stub_ext::StubExt stu;
+    stu.set_lamda(&GrandSearchListDelegate::calcTailShowData, [](){
+        QMap<int, QString> map;
+        map.insert(1, "aaa");
+        map.insert(2, "bbb");
+        return map;
+    });
+
+    bool draw = false;
+    stu.set_lamda(&GrandSearchListDelegate::drawTailDetailedInfo, [&](){
+        draw = true;
+    });
+
+    QPainter painter;
+    view.m_delegate->drawTailText(&painter, option, index, textMaxWidth, actualStartx);
+    EXPECT_TRUE(draw);
+
+    view.onSetThemeType(DGuiApplicationHelper::LightType);
+    draw = false;
+    view.m_delegate->drawTailText(&painter, option, index, textMaxWidth, actualStartx);
+    EXPECT_TRUE(draw);
+}
+
+TEST(GrandSearchListDelegateTest, drawTailDetailedInfo)
+{
+    QPainter painter;
+    QStyleOptionViewItem option;
+    QString text;
+    QColor color;
+    QFont font;
+    int startX;
+
+    text = "aaa";
+    color = QColor("#FFFFFF");
+    font = DFontSizeManager::instance()->get(DFontSizeManager::T8);
+    QFontMetrics fontMetrics(font);
+    startX = 100;
+    option.rect.setHeight(50);
+
+    stub_ext::StubExt stu;
+    bool ut_call_save = false;
+    stu.set_lamda(&QPainter::save, [&](){
+        ut_call_save = true;
+    });
+
+    bool ut_call_restore = false;
+    stu.set_lamda(&QPainter::restore, [&](){
+        ut_call_restore = true;
+    });
+
+    GrandSearchListView view;
+    view.m_delegate->drawTailDetailedInfo(&painter, option, text, color, font, fontMetrics, startX);
+    EXPECT_TRUE(ut_call_save);
+    EXPECT_TRUE(ut_call_restore);
+}
+
+TEST(GrandSearchListDelegateTest, calcTailShowInfo)
+{
+    int totalTailWidth = 500;
+    int tailCount = 2;
+    int averageWidth = 100;
+    GrandSearchListView view;
+    view.m_delegate->calcTailShowInfo(totalTailWidth, tailCount, averageWidth);
+    EXPECT_EQ(245, averageWidth);
+
+    totalTailWidth = 5;
+    averageWidth = 10;
+    view.m_delegate->calcTailShowInfo(totalTailWidth, tailCount, averageWidth);
+    EXPECT_EQ(totalTailWidth, averageWidth);
+}
+
+TEST(GrandSearchListDelegateTest, calcTailShowData)
+{
+    QStringList strings;
+    int tailCount;
+    int averageWidth;
+    QFont font;
+    QFontMetrics fontMetrics(font);
+
+    averageWidth = 200;
+
+    stub_ext::StubExt stu;
+    QMap<int, QString> map1;
+    map1.insert(1, "aaa");
+    stu.set_lamda(&GrandSearchListDelegate::calcTailShowDataByMaxWidth, [&](){
+        return map1;
+    });
+
+    QMap<int, QString> map2;
+    map2.insert(2, "bbb");
+    stu.set_lamda(&GrandSearchListDelegate::calcTailShowDataByOptimalWidth, [&](){
+        return map2;
+    });
+
+    GrandSearchListView view;
+    QMap<int, QString> result = view.m_delegate->calcTailShowData(strings, tailCount, averageWidth, fontMetrics);
+    EXPECT_EQ(result, map1);
+
+    averageWidth = 100;
+    result = view.m_delegate->calcTailShowData(strings, tailCount, averageWidth, fontMetrics);
+    EXPECT_EQ(result, map2);
+}
+
+TEST(GrandSearchListDelegateTest, calcTailShowDataByMaxWidth)
+{
+    QStringList strings;
+    int tailCount = 2;
+    int averageWidth;
+    QFont font;
+    QFontMetrics fontMetrics(font);
+
+    strings.append("aaa");
+    strings.append("aaaaaaaaaaaaaa");
+    averageWidth = 100;
+
+    stub_ext::StubExt stu;
+    stu.set_lamda(&QFontMetrics::elidedText, [](){
+        QString s = "aa";
+        return s;
+    });
+
+    QMap<int, QString> map;
+    map.insert(0, "aa");
+    map.insert(1, "aa");
+
+    GrandSearchListView view;
+    QMap<int, QString> result = view.m_delegate->calcTailShowDataByMaxWidth(strings, tailCount, averageWidth, fontMetrics);
+    EXPECT_EQ(result, map);
+}
+
+TEST(GrandSearchListDelegateTest, calcTailShowDataByOptimalWidth)
+{
+    QStringList strings;
+    int tailCount;
+    int averageWidth;
+    QFont font;
+    QFontMetrics fontMetrics(font);
+
+    strings.append("aa");
+    strings.append("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+                   "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+                   "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+                   "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+    tailCount = 2;
+    averageWidth = 160;
+
+    QMap<int, QString> map;
+    map.insert(0, "aa");
+    map.insert(1, "a");
+
+    stub_ext::StubExt stu;
+    stu.set_lamda(&QFontMetrics::elidedText, [](){
+        QString s = "a";
+        return s;
+    });
+
+    GrandSearchListView view;
+    QMap<int, QString> result = view.m_delegate->calcTailShowDataByOptimalWidth(strings, tailCount, averageWidth, fontMetrics);
+    EXPECT_EQ(result, map);
+
+    stu.set_lamda(&QFontMetrics::elidedText, [](){
+       QString s = "ccccccccccccccccccccccccc";
+       return s;
+    });
+    strings.clear();
+    strings.append("ccccccccccccccccccccccccc");
+    strings.append("a");
+    tailCount = 2;
+    result = view.m_delegate->calcTailShowDataByOptimalWidth(strings, tailCount, averageWidth, fontMetrics);
+    map.clear();
+    map.insert(0, "ccccccccccccccccccccccccc");
+    map.insert(1, "a");
+    EXPECT_EQ(map, result);
 }
