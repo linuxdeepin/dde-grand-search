2c6a4b6d12ad4f8b0b2796c5693d470b6f8cf50d
Liu Zhangjian <liuzhangjian@uniontech.com>
2025-05-08 19:07:00 +0800

    feat: remove FSearch integration and related files
    - Deleted FSearch related source and header files to streamline the project.
    - Updated CMake configuration to remove references to FSearch.
    - Adjusted searcher implementation to focus on the new file name searcher.
    
    Log: clean up project by removing obsolete FSearch integration

--- src/dde-grand-search-daemon/configuration/configer.cpp
+++ src/dde-grand-search-daemon/configuration/configer.cpp
@@ -30,16 +30,11 @@ ConfigerPrivate::ConfigerPrivate(Configer *parent)
 UserPreferencePointer ConfigerPrivate::defaultSearcher()
 {
     QVariantHash data = {
-        #ifdef ENABLE_DEEPINANYTHING
                         {GRANDSEARCH_CLASS_FILE_DEEPIN, true},
-        #endif
-        #ifdef ENABLE_FSEARCH
-                         {GRANDSEARCH_CLASS_FILE_FSEARCH, true},
-        #endif
-                         {GRANDSEARCH_CLASS_APP_DESKTOP, true},
-                         {GRANDSEARCH_CLASS_SETTING_CONTROLCENTER, true},
-                         {GRANDSEARCH_CLASS_WEB_STATICTEXT, true},
-                         {GRANDSEARCH_CLASS_GENERALFILE_SEMANTIC, true}
+                        {GRANDSEARCH_CLASS_APP_DESKTOP, true},
+                        {GRANDSEARCH_CLASS_SETTING_CONTROLCENTER, true},
+                        {GRANDSEARCH_CLASS_WEB_STATICTEXT, true},
+                        {GRANDSEARCH_CLASS_GENERALFILE_SEMANTIC, true}
                         };
 
     return UserPreferencePointer(new UserPreference(data));
@@ -103,7 +98,6 @@ bool ConfigerPrivate::updateConfig1(QSettings *set)
     //文件搜索相关配置
     {
         //初始化文件搜索的子类目
-    #ifdef ENABLE_DEEPINANYTHING
         if (UserPreferencePointer conf = m_root->group(GRANDSEARCH_CLASS_FILE_DEEPIN)) {
             //若所有的文件类搜索都关闭，则关闭文件搜索项
             bool on = false;
@@ -136,42 +130,6 @@ bool ConfigerPrivate::updateConfig1(QSettings *set)
         } else {
             qWarning() << "no shuch config:" << GRANDSEARCH_CLASS_FILE_DEEPIN;
         }
-    #endif
-
-    #ifdef ENABLE_FSEARCH
-        if (UserPreferencePointer conf = m_root->group(GRANDSEARCH_CLASS_FILE_FSEARCH)) {
-            //若所有的文件类搜索都关闭，则关闭文件搜索项
-            bool on = false;
-            bool ret = set->value(GRANDSEARCH_GROUP_FOLDER, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FOLDER, ret);
-            on |= ret;
-
-            ret = set->value(GRANDSEARCH_GROUP_FILE, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FILE, ret);
-            on |= ret;
-
-            ret = set->value(GRANDSEARCH_GROUP_FILE_VIDEO, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FILE_VIDEO, ret);
-            on |= ret;
-
-            ret = set->value(GRANDSEARCH_GROUP_FILE_AUDIO, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FILE_AUDIO, ret);
-            on |= ret;
-
-            ret = set->value(GRANDSEARCH_GROUP_FILE_PICTURE, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FILE_PICTURE, ret);
-            on |= ret;
-
-            ret = set->value(GRANDSEARCH_GROUP_FILE_DOCUMNET, true).toBool();
-            conf->setValue(GRANDSEARCH_GROUP_FILE_DOCUMNET, ret);
-            on |= ret;
-
-            //设置是否启用文件搜索项
-            searcherConfig->setValue(GRANDSEARCH_CLASS_FILE_FSEARCH, on);
-        } else {
-            qWarning() << "no shuch config:" << GRANDSEARCH_CLASS_FILE_FSEARCH;
-        }
-    #endif
     }
 
     //设置是否启用设置搜索项
@@ -367,13 +325,7 @@ void Configer::initDefault()
     rootData.insert(GRANDSEARCH_PREF_SEARCHERENABLED, QVariant::fromValue(d->defaultSearcher()));
 
     //初始化文件搜索的子类目
-#ifdef ENABLE_DEEPINANYTHING
     rootData.insert(GRANDSEARCH_CLASS_FILE_DEEPIN, QVariant::fromValue(d->fileSearcher()));
-#endif
-
-#ifdef ENABLE_FSEARCH
-    rootData.insert(GRANDSEARCH_CLASS_FILE_FSEARCH, QVariant::fromValue(d->fileSearcher()));
-#endif
 
     // 拖尾
     rootData.insert(GRANDSEARCH_TAILER_GROUP, QVariant::fromValue(d->tailerData()));
