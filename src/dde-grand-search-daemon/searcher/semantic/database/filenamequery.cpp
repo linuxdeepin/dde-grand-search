564a3d851e087e2e524f8832946e7a03a10d6fc1
Liu Zhangjian <liuzhangjian@uniontech.com>
2025-05-09 11:17:25 +0800

    feat: refactor search result processing in FileNameQuery
    - Introduced a new enum ProcessResult to handle search outcomes more effectively.
    - Updated processSearchResults method to process individual file results and return appropriate status.
    - Enhanced searchByDFMSearch to handle search result validation and termination conditions.
    - Increased maximum search results limit for improved performance.
    
    Log: improve search result handling and processing logic in FileNameQuery

--- src/dde-grand-search-daemon/searcher/semantic/database/filenamequery.cpp
+++ src/dde-grand-search-daemon/searcher/semantic/database/filenamequery.cpp
@@ -9,40 +9,44 @@
 using namespace GrandSearch;
 DFM_SEARCH_USE_NS
 
-#define MAX_SEARCH_NUM_TOTAL 10000
-
 FileNameQueryPrivate::FileNameQueryPrivate()
 {
 }
 
 void FileNameQueryPrivate::searchByDFMSearch(PushItemCallBack callBack, void *pdata)
 {
-    for (const SemanticEntity &entity : m_entity) {
+    for (const SemanticEntity &entity : std::as_const(m_entity)) {
         if (m_handler && m_handler->isResultLimit())
-            break;
+            return;
 
         QObject holder;
         SearchEngine *engine = SearchFactory::createEngine(SearchType::FileName, &holder);
-
         // 创建搜索选项
         SearchOptions options = createSearchOptions(entity);
         // 创建搜索查询
         SearchQuery query = createSearchQuery(entity);
-
         // 配置文件名选项
         FileNameOptionsAPI fileNameOptions(options);
         configureFileNameOptions(fileNameOptions, query, entity);
-
         // 设置搜索选项
         engine->setSearchOptions(options);
-
         // 执行搜索并处理结果
         const SearchResultExpected &result = engine->searchSync(query);
-        if (!processSearchResults(result, entity, callBack, pdata))
-            return;
+        if (!result.hasValue()) {
+            qWarning() << "DFMSearch failed for key: " << entity.keys << result.error().message();
+            continue;
+        }
+
+        for (const auto &file : result.value()) {
+            auto ret = processSearchResult(file.path(), entity, callBack, pdata);
+            if (ret == Terminated)
+                return;
+            else if (ret == Invalid)
+                continue;
+        }
     }
 
-    callBack(m_resultItems, pdata);
+    callBack(std::move(m_resultItems), pdata);
 }
 
 void FileNameQueryPrivate::configureFileNameOptions(FileNameOptionsAPI &fileNameOptions, const SearchQuery &query, const SemanticEntity &entity) const
@@ -59,48 +63,40 @@ void FileNameQueryPrivate::configureFileNameOptions(FileNameOptionsAPI &fileName
     }
 }
 
-bool FileNameQueryPrivate::processSearchResults(const SearchResultExpected &result,
-                                                const SemanticEntity &entity,
-                                                PushItemCallBack callBack,
-                                                void *pdata)
+FileNameQueryPrivate::ProcessResult FileNameQueryPrivate::processSearchResult(const QString &result,
+                                                                              const SemanticEntity &entity,
+                                                                              PushItemCallBack callBack,
+                                                                              void *pdata)
 {
-    if (!result.hasValue()) {
-        qWarning() << "DFMSearch failed for key: " << entity.keys << result.error().message();
-        return false;
+    // 检查是否需要推送数据与中断
+    if (timeToPush()) {
+        auto it = std::move(m_resultItems);
+        bool ret = callBack(it, pdata);
+        // 有数据放入，更新时间
+        if (!it.isEmpty())
+            m_lastPush = m_time.elapsed();
+
+        // 中断
+        if (!ret)
+            return Terminated;
     }
 
-    for (const auto &file : result.value()) {
-        // 检查是否需要推送数据与中断
-        if (timeToPush()) {
-            auto it = std::move(m_resultItems);
-            bool ret = callBack(it, pdata);
-            // 有数据放入，更新时间
-            if (!it.isEmpty())
-                m_lastPush = m_time.elapsed();
+    if (!checkResultValid(result, entity))
+        return Invalid;
 
-            // 中断
-            if (!ret)
-                return false;
-        }
-
-        const auto result = file.path();
-        if (!checkResultValid(result, entity))
-            continue;
-
-        m_count++;
-        if (m_handler) {
-            m_handler->appendTo(result, m_resultItems);
-            const auto fileName = result.mid(result.lastIndexOf('/') + 1);
-            m_handler->setItemWeight(result, m_handler->itemWeight(result) + calcItemWeight(fileName));
-            if (m_handler->isResultLimit() || m_count >= 100)
-                break;
-        } else {
-            auto item = FileSearchUtils::packItem(result, GRANDSEARCH_CLASS_GENERALFILE_SEMANTIC);
-            m_resultItems[GRANDSEARCH_GROUP_FILE_INFERENCE].append(item);
-        }
+    m_count++;
+    if (m_handler) {
+        m_handler->appendTo(result, m_resultItems);
+        const auto fileName = result.mid(result.lastIndexOf('/') + 1);
+        m_handler->setItemWeight(result, m_handler->itemWeight(result) + calcItemWeight(fileName));
+        if (m_handler->isResultLimit() || m_count >= 100)
+            return Terminated;
+    } else {
+        auto item = FileSearchUtils::packItem(result, GRANDSEARCH_CLASS_GENERALFILE_SEMANTIC);
+        m_resultItems[GRANDSEARCH_GROUP_FILE_INFERENCE].append(item);
     }
 
-    return true;
+    return Success;
 }
 
 bool FileNameQueryPrivate::checkResultValid(const QString &result, const SemanticEntity &entity)
@@ -166,7 +162,6 @@ SearchOptions FileNameQueryPrivate::createSearchOptions(const SemanticEntity &en
     SearchOptions options;
     options.setSearchPath(QDir::rootPath());
     options.setSearchMethod(SearchMethod::Indexed);
-    options.setMaxResults(MAX_SEARCH_NUM_TOTAL);
     return options;
 }
 
